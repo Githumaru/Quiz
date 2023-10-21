867811078029a675d7224d4e89cec5fb36a04793
Vyacheslav <trofimov_vo@mail.ru>
2023-10-21 15:42:50 +0400

    show questions

--- quiz/views.py
+++ quiz/views.py
@@ -4,6 +4,7 @@ import requests
 from datetime import datetime
 
 def create_quiz_entry(request, questions_num):
+    created_entries = []
     for _ in range(questions_num):
         while True:
             response = requests.get('https://jservice.io/api/random?count=1')
@@ -11,8 +12,12 @@ def create_quiz_entry(request, questions_num):
             if not Quiz.objects.filter(question=data['question']).exists():
                 quiz = Quiz(id=data['id'], question=data['question'], answer=data['answer'], created_at=datetime.now())
                 quiz.save()
+                created_entries.append(f"id: {quiz.id}"
+                            f"\nquestion: {quiz.question}"
+                            f"\nanswer: {quiz.answer}"
+                            f"\ncreated_at: {quiz.created_at.strftime('%d-%m-%Y %H:%M')}")
                 break
-    return HttpResponse("Quiz entries created successfully.", content_type="text/plain")
+    return HttpResponse(f"Quiz entries created successfully:\n" + "\n\n".join(created_entries), content_type="text/plain")
 
 def get_latest_quiz(request):
     try:
